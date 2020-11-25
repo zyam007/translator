f93886c4d629b57a81f29a3cd79acddb6bbef856
Jenna Jiang <tqj5047@gmail.comm>
2020-11-25 09:24:05 -0500

    changed file structure

---
+++ client/components/friendSearch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//search for friend, thunk to get friend, change status(requested, confirmed, blocked, denied)
+function friendSearch(props) {
+  return <div />
+}
+
+export default friendSearch
