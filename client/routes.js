c88798c6415381a6e9f7591e6335d91899fdc48a
Josephine Amos <josephinexamos@gmail.com>
2020-11-30 15:25:39 -0500

    Feature: Deleted user-home, reconnected welcome and main

--- client/routes.js
+++ client/routes.js
@@ -35,7 +35,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route exact path="/" component={Welcome} />
             <Route path="/myprofile" component={MyProfile} />
             <Route path="/friendlist" component={FriendList} />
             <Route path="/main" component={Main} />
@@ -43,7 +43,7 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        <Route exact path="/" component={Welcome} />
       </Switch>
     )
   }
