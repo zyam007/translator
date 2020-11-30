96220633ae64253b87d2e19a91e90ab7b795f48c
Yanna Sk <zyam007@gmail.com>
2020-11-30 13:04:31 -0500

    botstrap styles are loaded too

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {NavBar} from './components'
 import Routes from './routes'
 //import '../public/App.scss'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const App = () => {
   return (
