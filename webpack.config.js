a87e1c7226000955172b8de41edabb3a3a303c8b
Yanna Sk <zyam007@gmail.com>
2020-11-30 13:16:34 -0500

    cleaned comments from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,32 +27,6 @@ module.exports = {
       },
       //original
       {test: /\.css$/i, use: ['style-loader', 'css-loader']}
-      // {
-      //   test: /\.s[ac]ss$/i,
-      //   use: [
-      //     // Creates `style` nodes from JS strings
-      //     'style-loader',
-      //     // Translates CSS into CommonJS
-      //     'css-loader',
-      //     // Compiles Sass to CSS
-      //     'sass-loader',
-      //   ],
-      // },
-
-      // {
-      //   test: /\.css$/i,
-      //   loader: 'css-loader',
-      //   options: {
-      //     import: (url, media, resourcePath) => {
-      //       // resourcePath - path to css file
-      //       // Don't handle `style.css` import
-      //       if (url.includes('style.css')) {
-      //         return false
-      //       }
-      //       return true
-      //     },
-      //   },
-      // },
     ]
   }
 }
