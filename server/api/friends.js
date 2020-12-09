358052e1036c46e7ce331d12ca14e68958a3c993
Yanna Sk <zyam007@gmail.com>
2020-12-09 12:17:44 -0500

    Feature: chat room input field and buttons changed, scroll to view works, isUser resused

--- server/api/friends.js
+++ server/api/friends.js
@@ -3,14 +3,6 @@ const {User, Message, Friendship, Conversation} = require('../db/models')
 const isUser = require('./isUser')
 module.exports = router
 
-const isUser = (req, res, next) => {
-  if (!req.user) {
-    res.sendStatus(401)
-    return
-  }
-  next()
-}
-
 router.get('/:id', isUser, async (req, res, next) => {
   const {id} = req.params
   try {
