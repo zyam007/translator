fff265bf1550b87eb729b7323f698a4b68df0cb8
Jenna <tqj5047@gmail.com>
2020-12-13 17:04:14 -0500

    added persist store

--- client/index.js
+++ client/index.js
@@ -4,16 +4,20 @@ import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
+import {persistor} from './store'
 import App from './app'
 import {Notifications} from 'react-push-notification'
 // establishes socket connection
 // import './socket'
-
+import {PersistGate} from 'redux-persist/lib/integration/react'
+import {LoadingView} from '../client/components/index'
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <Notifications />
-      <App />
+      <PersistGate loading={<LoadingView />} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById('app')
