d8b3650e29e8937d27df0bb5638bd50ce0dfda94
Jenna <tqj5047@gmail.com>
2020-12-13 18:09:55 -0500

    fixed routes secured

--- client/index.js
+++ client/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
-import store from './store'
-import {persistor} from './store'
+import store, {persistor} from './store'
 import App from './app'
 import {Notifications} from 'react-push-notification'
-// establishes socket connection
-// import './socket'
 import {PersistGate} from 'redux-persist/lib/integration/react'
 import {LoadingView} from '../client/components/index'
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
